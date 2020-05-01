0c56133e69c8806cbf5c2d0523083f2b1d4a15a0
Dom Del Nano <ddelnano@gmail.com>
2020-05-01 09:36:45 -0700

    Finish implementation

--- providers/mikrotik/dhcp_leases.go
+++ providers/mikrotik/dhcp_leases.go
@@ -1,6 +1,21 @@
+// Copyright 2019 The Terraformer Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package mikrotik
 
 import (
+	"fmt"
+
 	"github.com/GoogleCloudPlatform/terraformer/terraform_utils"
 	"github.com/ddelnano/terraform-provider-mikrotik/client"
 )
@@ -12,10 +27,13 @@ type DhcpLeaseGenerator struct {
 func (g DhcpLeaseGenerator) createResources(leases []client.DhcpLease) []terraform_utils.Resource {
 	var resources []terraform_utils.Resource
 	for _, lease := range leases {
+		resourceName := lease.Id
+		if lease.Hostname != "" {
+			resourceName = fmt.Sprintf("%s-%s", lease.Hostname, lease.Id)
+		}
 		resources = append(resources, terraform_utils.NewSimpleResource(
 			lease.Id,
-			// # TODO: prefer hostname if set, otherwise use Id
-			lease.Id,
+			resourceName,
 			"mikrotik_dhcp_lease",
 			"mikrotik",
 			[]string{}))
